4adb9da0bd9ae451fad7db7b69fc4c0790e9497c
PR!nce <108923246+maihoonprince@users.noreply.github.com>
2023-02-05 00:48:27 +0530

    Add files via upload

---
+++ benjaminBulb.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class benjaminBulb 
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i=1; i*i <= n; i++)
+        {
+            System.out.println(i*i);
+        }
+
+    }
+    
+}
